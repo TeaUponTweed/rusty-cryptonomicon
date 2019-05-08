44bd0a2996bf4b1293e35a7228f81ea679371b0c
Michael Mason <TeaUponTweed@gmail.com>
2019-05-07 21:05:46 -0600

    initial json parsing

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,45 @@
-// extern crate clap;
-// extern crate serde;
-// extern crate serde_json;
-
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-// use clap::App; 
 use clap::{App, Arg, SubCommand};
 
 use serde::Deserialize;
-use serde_json::Result;
+// use serde_json::Result;
 
 
 
+// #[serde(rename_all = "camelCase")]
 #[derive(Debug, Deserialize)]
 struct TradingPair {
     exchange: String,
-    baseAsset: String,
     quoteAsset: String,
-    exchangeRate: f32,
+    baseAsset: String,
+    rate: f32,
     capacity: f32,
 }
 
 
-fn main() {
-    // // Open the path in read-only mode, returns `io::Result<File>`
-    // let mut file = match File::open(&path) {
-    //     // The `description` method of `io::Error` returns a string that
-    //     // describes the error
-    //     Err(why) => panic!("couldn't open {}: {}", display,
-    //                                                why.description()),
-    //     Ok(file) => file,
-    // };
 
-    // // Read the file contents into a string, returns `io::Result<usize>`
-    // let mut s = String::new();
-    // match file.read_to_string(&mut s) {
-    //     Err(why) => panic!("couldn't read {}: {}", display,
-    //                                                why.description()),
-    //     Ok(_) => print!("{} contains:\n{}", display, s),
+fn load_trading_pairs(path: &Path) -> TradingPair {
+    let mut file = File::open(path).expect("Trading pairs file not found! Exiting");
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+    let trading_pairs: TradingPair =
+        serde_json::from_str(&data).expect("JSON was not well-formatted");
+    trading_pairs
+}
+
+fn optimize_rate(trading_pair_file: &str, starting_asset: &str, final_asset: &str) {
+    let trading_pairs = load_trading_pairs(Path::new(trading_pair_file));
+    println!("Converting {}->{}", starting_asset, final_asset);
+    println!("{:?}", trading_pairs);
+    // for tp in trading_pairs {
+    //     println!("{:?}", tp);
     // }
-    // App::new("SALT Coding Challenge")
-    //     .arg(Arg::with_name("trading pairs")
-    //                 .help("JSON file containing a list of trading pairs")
-    //                 .index(1)
-    //                 .required(true)
-    //     )
-    //     .arg(Arg::with_name("from")
-    //                 .help("Starting asset")
-    //                 .index(2)
-    //                 .required(true)
-    //     .arg(Arg::with_name("to")
-    //                 .help("Final asset")
-    //                 .index(3)
-    //                 .required(true)
-    //     )
-    //     .arg(Arg::with_name("optimize rate")
-    //                 .help("Optimize the rate of conversion, ignores capacity")
-    //                 .long("optimize-rate")
-    //                 .conflicts_with("optimize net")
-    //     )
-    //     .arg(Arg::with_name("optimize net")
-    //                 .help("Optimize the net amount of the final asset.")
-    //                 .long("optimize-net")
-    //                 .conflicts_with("optimize rate")
-    //                 .requires("asset quantity")
-    //     )
-    //    .about("Given arbitrary crypto-assets --from and --to,"
-    //           " this finds a trading-pair chain with the most favorable aggregate exchange rate")
-    //    .author("Michael Mason")
-    //    .get_matches(); 
+}
+
+fn main() {
     let matches = App::new("cryptoptim")
         .subcommand(SubCommand::with_name("net")
             .about("Optimize the net amount of the final asset")
@@ -110,13 +79,28 @@ fn main() {
             .arg(Arg::with_name("to")
                 .help("Destination asset")
                 .index(3)
+                .required(true)
             )
         )
        .get_matches();
 
-    match matches.subcommand_name() {
-        Some("net")  => println!("'cryptoptim add' was used"),
-        Some("rate") => println!("'cryptoptim add' was used"),
-        _            => println!("Use either \"net\" or \"rate\" subcommand. See help (-h) for details."),
+    match matches.subcommand() {
+        ("rate", Some(rate_matches))  => {
+            optimize_rate(
+                rate_matches.value_of("trading pairs").unwrap(),
+                rate_matches.value_of("from").unwrap(),
+                rate_matches.value_of("to").unwrap()
+            );
+        },
+        ("net", Some(net_matches)) => {
+            let quantity = net_matches.value_of("asset quantity").unwrap();
+            if let Some(quantity) = quantity.parse::<f32>().ok() {
+                println!("'cryptoptim net' was used with quantity = {:?}", quantity);
+            }
+            else {
+                println!("Failed to parse \"{}\" into a float! Exiting now", quantity);
+            }
+        },
+        _ => println!("Use either \"net\" or \"rate\" subcommand. See help (-h) for details."),
     }
 }
